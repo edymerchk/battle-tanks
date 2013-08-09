201ee72ad33c918ce45b8f991aa169500d5beb21
edymerchk <edyhack@gmail.com>
2013-08-08 21:24:53 -0500

    bullet

--- js/battle-tanks.js
+++ js/battle-tanks.js
@@ -3,31 +3,41 @@ $(function() {
  var socket = io.connect('http://localhost', {port: 8080});
 
 
-var left = function(){
-  console.log("lef");
-  toX = $('#tank').position().left - 50
-  $('#tank').css('left', toX)
-}
+  var left = function(){
+    console.log("lef");
+    toX = $('#tank').position().left - 50
+    $('#tank').css('left', toX)
+  }
 
-var right = function(){
-  console.log("right");
-   toX = $('#tank').position().left + 50
-  $('#tank').css('left', toX)
-   
-}
-
-
-var angle = function (value) {
-  val = (value.value - 512)/4
-  console.log(val)
-  $("#tank").css({ WebkitTransform: 'rotate(' + val + 'deg)'});
-}
+  var right = function(){
+    console.log("right");
+     toX = $('#tank').position().left + 50
+    $('#tank').css('left', toX)
+     
+  }
 
 
-socket.on('left', left);
-socket.on('right', right);
-socket.on('angle', function (data) {
-  angle(data);
-});
+  var angle = function (value) {
+    val = (value.value - 512)/4
+    $("#tank").css({ WebkitTransform: 'rotate(' + val + 'deg)'});
+  }
+
+
+  socket.on('left', left);
+  socket.on('right', right);
+  socket.on('angle', function (data) {
+    angle(data);
+    shoot();
+  });
+
+
+
+  var shoot = function (angle) {
+    setInterval(function(){
+      var imgpos = $('#bullet').offset().left;
+      var imgwidth = $('#bullet').width();
+      console.log(imgpos)
+    },1000)  
+  }
 
 });
